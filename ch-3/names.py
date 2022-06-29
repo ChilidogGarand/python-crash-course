c0e1883694d8a22f7db74c671c0d9ef856a1f23f
ChilidogGarand <jamx42@protonmail.com>
2022-06-29 14:59:16 -0500

    Made a silly little program to greet the people i work with

---
+++ ch-3/names.py
@@ -0,0 +1,13 @@
+import random
+
+# Some totally random names
+friends = ['Josh', 'Nevin', 'Andrew', 'Steve', 'Caitlyn', 'Zeke', 'Nathan']
+# An equal number of greetings
+greetings = ["What's up, ", "How's it going, ", "Hey there, ", "What the haps, ", "How you doin', ", "Oh hi there, ", "Eat it, "]
+# Random integers to choose one of each from above
+random1 = random.randint(0, 6)
+random2 = random.randint(0, 6)
+
+# And they both go together in the secret sauce.
+while __name__ == __main__: (
+    print(random1 + random2)  )
