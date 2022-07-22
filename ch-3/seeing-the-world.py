4c6ecafacea3faf885d89528107ae8d65407a1d0
Jakers <jamx42@protonmail.com>
2022-07-22 11:45:14 -0500

    Exercise 3-8 completed

---
+++ ch-3/seeing-the-world.py
@@ -0,0 +1,30 @@
+# List filled with bucket list locations I'd like to go see
+bucketlist_d = ['seattle', 'prague', 'london', 'oaxaca', 'maldives', 'scotland', 'barcelona', 'berlin']
+
+# Print raw list in original order
+print(bucketlist_d)
+
+# Print list with sorted function
+print(sorted(bucketlist_d))
+
+# Print list in original order again to show it has been retained
+print(bucketlist_d)
+
+# Reverse order of the list 
+bucketlist_d.reverse()
+print(bucketlist_d)
+
+# Reverse it again to restore order to the universe.
+bucketlist_d.reverse()
+print(bucketlist_d)
+
+# Sort the list in alphabetical order permanently
+bucketlist_d.sort()
+print(bucketlist_d)
+
+# Then sort it again to reverse alphabetical order
+bucketlist_d.sort(reverse=True)
+print(bucketlist_d)
+
+
+
