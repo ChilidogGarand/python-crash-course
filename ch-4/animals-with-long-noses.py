5750fb898f5ae74700a59fd8f8206772ab4c3c9a
chilidoggarand <jamx42@protonmail.com>
2022-07-23 11:33:50 -0500

    exercise 4-2

---
+++ ch-4/animals-with-long-noses.py
@@ -0,0 +1,4 @@
+animals = ['elephant', 'anteater', 'chupacabra probably']
+for animal in animals:
+    print(f"The {animal} has a pretty big nose.")
+print("All of these animals could probably do a shitload of cocaine real easy.")
