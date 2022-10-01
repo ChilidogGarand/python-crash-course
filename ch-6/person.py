6291ab4ea5ea6b081dd2e8f9da300f0b47bbdb3d
chilidoggarand <jamx42@protonmail.com>
2022-10-01 11:11:47 -0500

    exercise 6-1 completed

---
+++ ch-6/person.py
@@ -0,0 +1,12 @@
+# Exercise 6-1, p99
+
+chilidog = {
+    'first_name': 'chilidog',
+    'last_name': 'garand',
+    'age': '69',
+    'city': 'buckettown',
+    'state': 'texas',
+    }
+
+print(f"Let's talk about {chilidog['first_name'].title()} {chilidog['last_name'].title()}.")
+print(f"{chilidog['first_name'].title()} is {chilidog['age']} years old and lives in {chilidog['city'].title()}, {chilidog['state'].title()}.")
