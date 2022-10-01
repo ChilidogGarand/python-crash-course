101715e6048d2e807a72c05062f1b186435e196a
chilidoggarand <jamx42@protonmail.com>
2022-10-01 11:24:26 -0500

    completed exercise 6-2

---
+++ ch-6/favorite-numbers.py
@@ -0,0 +1,15 @@
+# Exercise 6-2, p99
+
+fav_numbers = {
+    'marsha': '10',
+    'jan': '1',
+    'greg': '7',
+    'peter': '69',
+    'bobby': '42',
+}
+
+print(f"Marsha's favorite number is {fav_numbers['marsha']}.")
+print(f"Jan's favorite number is {fav_numbers['jan']}.")
+print(f"Greg's favorite number is {fav_numbers['greg']}.")
+print(f"Peter's favorite number is {fav_numbers['peter']}.")
+print(f"Bobby's favorite number is {fav_numbers['bobby']}.")
