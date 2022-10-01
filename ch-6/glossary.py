a13f210cde3cca110146067a09bbfeb55307898f
chilidoggarand <jamx42@protonmail.com>
2022-10-01 11:37:42 -0500

    exercise 6-3

---
+++ ch-6/glossary.py
@@ -0,0 +1,15 @@
+# Exercise 6-3, p99
+
+python_terms = {
+    'list': "A collection of items in a particular order.",
+    'dictionary': "A collection of items stored in key-value pairs.",
+    'if statement': "A conditional statement that carries out its function when a condition is fulfilled.",
+    'elif statement': "A conditional statement that runs consecutively to an if statement, which carries out its function when the previous statement's execution condition is not fulfilled, as long as the execution of the elif statement is also fulfilled.",
+    'else statement': "A conditional statement that runs consecutively to an unfulfilled if statement (and after any unfulfilled elif statements) that carries out its function when all above statements have not been fulfilled. Often used for errors.",
+}
+
+print(f"list:\n{python_terms['list']}")
+print(f"dictionary:\n{python_terms['dictionary']}")
+print(f"if statement:\n{python_terms['if statement']}")
+print(f"elif statement:\n{python_terms['elif statement']}")
+print(f"else statement:\n{python_terms['else statement']}")
