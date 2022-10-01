c9092dd1fd0e34dd9085d2ba494de6fc24998f71
chilidoggarand <jamx42@protonmail.com>
2022-10-01 13:46:25 -0500

    working on exercise 6-4

--- ch-6/glossary.py
+++ ch-6/glossary.py
@@ -1,4 +1,5 @@
 # Exercise 6-3, p99
+# Revised for exercise 6-4, p105
 
 python_terms = {
     'list': "A collection of items in a particular order.",
@@ -8,8 +9,16 @@ python_terms = {
     'else statement': "A conditional statement that runs consecutively to an unfulfilled if statement (and after any unfulfilled elif statements) that carries out its function when all above statements have not been fulfilled. Often used for errors.",
 }
 
-print(f"list:\n{python_terms['list']}")
-print(f"dictionary:\n{python_terms['dictionary']}")
-print(f"if statement:\n{python_terms['if statement']}")
-print(f"elif statement:\n{python_terms['elif statement']}")
-print(f"else statement:\n{python_terms['else statement']}")
+# This was for exercise 6-3, commenting out so that I can write a new statement for exercise 6-4
+#print(f"list:\n{python_terms['list']}")
+#print(f"dictionary:\n{python_terms['dictionary']}")
+#print(f"if statement:\n{python_terms['if statement']}")
+#print(f"elif statement:\n{python_terms['elif statement']}")
+#print(f"else statement:\n{python_terms['else statement']}")
+
+# Defining the loop as a function that can be called, because that's way easier
+def list_terms():
+    for term,definition in python_terms.items():
+        print(f"{term}:\n{definition}\n")
+
+list_terms()
