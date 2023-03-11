bbaabf5247834e9b52aa17d3f41b04b08786f148
ChilidogGarand <jamx42@protonmail.com>
2023-03-11 12:08:15 -0600

    finished exercise 6-5

--- ch-6/rivers.py
+++ ch-6/rivers.py
@@ -1,14 +1,29 @@
 # Exercise 6-5, p105
 
+# The names of three rivers and the countries they run through as a dictionary
 rivers = {
     'nile': 'egypt',
     'mississippi': 'america',
     'thames': 'united kingdom',
 }
 
+# A fucntion that prints a sentence running through the rivers dictionary and 
+# stating the name of the river and the country it runs through.
 def print_a_sentence():
     for name, country in rivers.items():
         print(f"The {name.title()} river is in {country.title()}.")
-    
 
-print_a_sentence()
+# A function that prints the names of the rivers in title case    
+def print_rivers():
+    for name in rivers.keys():
+        print(f"{name.title()}")
+
+#A function that prints the countries by themselves in title case
+def print_countries():
+    for country in rivers.values():
+            print(f"{country.title()}")
+
+# Calling functions for individual parts of the exercise
+#print_a_sentence()
+#print_rivers()
+print_countries()
