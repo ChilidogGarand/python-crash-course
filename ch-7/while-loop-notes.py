e6f195d2f95438c84bd4f7b437fda421f6fbd82f
ChilidogGarand <jamx42@protonmail.com>
2023-07-15 13:45:57 -0500

    updated chapter 7 notes

--- ch-7/while-loop-notes.py
+++ ch-7/while-loop-notes.py
@@ -77,3 +77,69 @@ def count_continue():
         print(current_number)
 
 # count_continue()
+
+# Using a while loop with lists and dictionaries.
+# - A while loop can be used to keep track of many users and pieces of information
+#   in dictionaries and lists.
+# - A 'for' loop is effective for looping through a list, but modifying items
+#   within a 'for' loop can cause Python to have trouble keeping track of items
+#   within the list. 
+
+# Moving items from one list to another. 
+
+# Start with users that need to be verified, and add an empty list to hold confirmed
+# users
+def confirmed_users_moving():
+    unconfirmed_users = ['alice', 'brian', 'candace']
+    confirmed_users = []
+# Verify each user until there are no more confirmed users. Move each verified user
+# into the list of confirmed users
+    while unconfirmed_users:
+        current_user = unconfirmed_users.pop()
+        print(f"Verifying user: {current_user.title()}")
+        confirmed_users.append(current_user)
+# Display all the con firmed users.
+        print("\nThe following users have been confirmed:")
+        for confirmed_user in confirmed_users:
+            print(confirmed_user.title())
+
+# confirmed_users_moving()
+
+# Removing all instances of specific values from a list.
+# The remove() function can be used to delete all instances of a given value.
+# Coupling it with a while loop allows us to dictate how those instances are removed.
+
+def pet_removal():
+    pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+    print(pets)
+    while 'cat' in pets:
+        pets.remove('cat')
+    print(pets)
+
+# pet_removal()
+
+# You can make a prompt for input at each pass of the loop, then store the data
+# in a dictionary to connect the input with a specific value.
+
+def polling_dictionary():
+    responses = {}
+    # Set a flag to indicate polling is active.
+    polling_active = True
+    while polling_active:
+        # Prompt for each person's name and repsonse.
+        name = input("\nWhat is your name?")
+        response = input("\nWhat mountain would you like to climb someday?")
+        # Store the responses in a dictionary.
+        responses[name] = response
+        # Find out if anyone else is going to take the poll.
+        repeat = input("\nWould you like to let another person respond? (yes/no)")
+        if repeat == 'no' or 'n':
+            polling_active = False
+        elif repeat == 'yes' or 'y':
+            continue
+    # Once polling is complete, show the results.
+    print("\n--- POLL RESULTS ---")
+    for name, response in responses.items():
+        print(f"{name} would like to climb {response}.")
+
+polling_dictionary()
