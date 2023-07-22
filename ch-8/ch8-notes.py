7c0b978328464a535089ad5027de763759fccac8
ChilidogGarand <jamx42@protonmail.com>
2023-07-22 15:33:22 -0500

    more chapter 8 notes

--- ch-8/ch8-notes.py
+++ ch-8/ch8-notes.py
@@ -27,6 +27,58 @@ def greet_user():
 
 def greet_username(username):
     """Display a simple greeting, using the username"""
-    print(f"Hello, {username.title()}")
+    print(f"Hello, {username.title()}!")
 
-greet_username('chilidog')
+#greet_username('chilidog')
+
+# When we call the function, we assign a value within the parenthetical of the 
+# function to the variable established when the function was defined. This 
+# variable is called the parameter, and the value we assign to the variable is 
+# called the argument. The argument is passed from the function call to the 
+# function. 
+
+# Passing Arguments
+# A function can have multiple parameters, therefore a function call may need
+# multiple arguments. You can pass these in a number of ways.
+#
+# Positional arguments, which will need to be in the same order that the 
+# paramaters are written.abs
+#
+# Keyword arguments, where each argument conists of a variable name and a value.
+#
+# Lists and dictionaries of values.
+
+# Positional Arguments
+# The simplest way to pass arguments to multiple parameters is to match each
+# argument in the function call with a parameter in the function definition in 
+# the same order. This is called a positional argument.
+
+def describe_pet(animal_type, pet_name):
+    # The definition of this function states that is needs a type of animal and an
+    # animal name. 
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+# When we call describe_pet(), we need to provide an animal type and a name in
+# that order. The following arguments describe a hamster named Harry in the 
+# function call.
+# describe_pet('hamster', 'harry')
+
+# Multiple Function Calls
+# You can call a function as many times as needed. For example, we can add another 
+# function call to describe another pet.
+
+#describe_pet('hamster', 'harry')
+#describe_pet('dog', 'willie')
+
+# Calling a function multiple times is a very efficient way to work. The code is
+# written once, and then whenever we want to describe a new pet, we can just
+# add another function call with the new pet's information. Even if we were
+# to expand the code, we could still describe a new pet in one line just by calling
+# the function again.
+
+# Order matters in positional arguments. If we mix up the order of the arguments,
+# unexpected results can occur.
+
+describe_pet('harry', 'hamster')
