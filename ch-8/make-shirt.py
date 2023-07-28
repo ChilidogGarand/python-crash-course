2ccb563d36b355ed37f3c8817f32424719bd4fc3
ChilidogGarand <jamx42@protonmail.com>
2023-07-28 14:54:01 -0500

    completed exercise 8-16

--- ch-8/make-shirt.py
+++ ch-8/make-shirt.py
@@ -5,8 +5,8 @@
 # function once using positional arguments to make a shirt. Call the function
 # a second time using keyword arguments.
 
-def make_shirt(size, message):
-    print(f'\nOkay, making you a size {size} shirt that says "{(message.capitalize())}".')
+#def make_shirt(size, message):
+#    print(f'\nOkay, making you a size {size} shirt that says "{(message.capitalize())}".')
 
 #make_shirt("large", "don't panic")
 #make_shirt(size='large',message="don't panic")
@@ -16,9 +16,34 @@ def make_shirt(size, message):
 # message that says "I love Python". Make a large shirt and a medium shirt
 # with the default method, and a shirt of any size with a different message.
 
-def make_shirt_def(size="large", message="i love python"):
-    print(f'\nOkay, making you a size {size} shirt that says "{(message.capitalize())}".')
+#def make_shirt_def(size="large", message="i love python"):
+#    print(f'\nOkay, making you a size {size} shirt that says "{(message.capitalize())}".')
 
 #make_shirt_def()
 #make_shirt_def(size="medium")
 #make_shirt_def(size="extra large", message="don't panic")
+
+# Exercise 8-16
+# Using a program you wrote that has one function in it, store that function in
+# a separate file. Import the function into your main program file, and call
+# the function using each of these three approaches.
+# import module_name
+# from module_name import function_name
+# from module_name import function_name as fn
+# import module_name as mn
+# from module_name import *
+
+#import shirt_functions
+#shirt_functions.make_shirt_def('large')
+
+#from shirt_functions import make_shirt_def
+#make_shirt_def('double XL', "don't panic")
+
+#from shirt_functions import make_shirt_def as ms
+#ms('small', "i'm just a widdle guy")
+
+#import shirt_functions as sf
+#sf.make_shirt_def('medium', "I went to milliways and all I got was a huge hangover")
+
+from shirt_functions import *
+make_shirt_def('XL', 'that feng fengin')
---
+++ ch-8/shirt_functions.py
@@ -0,0 +1,2 @@
+def make_shirt_def(size="large", message="i love python"):
+    print(f'\nOkay, making you a size {size} shirt that says "{(message.capitalize())}".')
