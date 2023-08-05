5ddcd4cb51dc7ea497c4cfb92b61ee7dc93f88e3
ChilidogGarand <jamx42@protonmail.com>
2023-08-05 12:24:03 -0500

    completed exercise 9-15

--- ch-9/lottery.py
+++ ch-9/lottery.py
@@ -17,13 +17,13 @@ class Ticket:
     def generate_ticket(self):
         """Generates a random ticket ticket with 4 values."""
         while len(self.number) < 4:
-            new_number = self.draw_pile.pop(random.randint(0,int(len(self.draw_pile) - 1)))
+            new_number = self.draw_pile[:].pop(random.randint(0,int(len(self.draw_pile) - 1)))
             self.number.append(str(new_number))
         self.number.sort()
         for lotto_number in self.number:
             self.formatted_number += f"{lotto_number} "
             
-        print(f"Your lottery numbers are {self.formatted_number}.")
+        print(f"Your lottery numbers are: {self.formatted_number}.")
 
     def run_lotto(self):
         """
@@ -31,11 +31,33 @@ class Ticket:
         Prints a message stating how many times the loop had to run until the 
         generated ticket won.
         """
-        
+        winning_number = []
+        formatted_winning_number = ""
+        have_won = False
+        current_loop = 0
+        while have_won == False:
+            while len(winning_number) < 4:
+                drawn_number = self.draw_pile[:].pop(random.randint(0,int(len(self.draw_pile) - 1)))
+                winning_number.append(str(drawn_number))
+            winning_number.sort()
+            for win_char in winning_number:
+                    formatted_winning_number += f"{win_char} "
+            if formatted_winning_number != self.formatted_number:
+                current_loop += 1
+                print(f"The winning number was {formatted_winning_number}. Your numbers {self.formatted_number} did not win.")
+                winning_number = []
+                formatted_winning_number = ""
+                continue
+            elif formatted_winning_number == self.formatted_number:
+                print(f"Your numbers {self.formatted_number} won after {str(current_loop)} drawings.")
+                have_won = True
+            
+
 
 my_ticket = Ticket()
 my_ticket.generate_ticket()
 
+my_ticket.run_lotto()
 # Exercise 9-15, p180
 # Lottery Analysis
 # You can use a loop to see how hard it might be to win the kind of lottery you
