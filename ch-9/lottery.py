6d498528a1dde1e4c0db747be06a63fc1069a6bf
ChilidogGarand <jamx42@protonmail.com>
2023-08-05 12:22:00 -0500

    completed exercise 9-15

---
+++ ch-9/lottery.py
@@ -0,0 +1,44 @@
+# Excercise 9-14, p180
+# Make a list or tupe containing a series of 10 numbers and five letters. 
+# Randomly select four numbers or letters from the list and print a message
+# saying that any ticket matching these four numbers or letters wins a prize.
+
+import random
+
+class Ticket:
+    """An object representing a lottery ticket with 4 random values"""
+
+    def __init__(self):
+        """Initialize the attributes of the lottery ticket."""
+        self.draw_pile = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E']
+        self.number = []
+        self.formatted_number = ""
+
+    def generate_ticket(self):
+        """Generates a random ticket ticket with 4 values."""
+        while len(self.number) < 4:
+            new_number = self.draw_pile.pop(random.randint(0,int(len(self.draw_pile) - 1)))
+            self.number.append(str(new_number))
+        self.number.sort()
+        for lotto_number in self.number:
+            self.formatted_number += f"{lotto_number} "
+            
+        print(f"Your lottery numbers are {self.formatted_number}.")
+
+    def run_lotto(self):
+        """
+        Runs a random drawing until the drawn number matches the generated ticket.
+        Prints a message stating how many times the loop had to run until the 
+        generated ticket won.
+        """
+        
+
+my_ticket = Ticket()
+my_ticket.generate_ticket()
+
+# Exercise 9-15, p180
+# Lottery Analysis
+# You can use a loop to see how hard it might be to win the kind of lottery you
+# just modeled. Make a list or a tuple called my_ticket. Write a loop that keeps
+# pulling numbers until your ticket wins. Print a message reporting how many
+# times the loop had to run to give you a winning ticket.
